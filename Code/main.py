2d612c1fbcd3c55def05039065bc62144e2273f4
Nikita <nikpar3l54@gmail.com>
2022-10-23 02:44:13 +0700

    Test_sql

--- Code/main.py
+++ Code/main.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+
+class Items(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)
+    description = db.Column(db.Text, nullable = False)
+    price = db.Column(db.Integer, nullable = False)
+
+
 
 @app.route('/')
 def index():
